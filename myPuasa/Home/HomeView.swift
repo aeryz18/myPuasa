a708b6a4bb3057507369f9df562483068393f6bd
aery <2023239802@student.uitm.edu.my>
2026-08-11 01:41:12 +0800

    home page layout

--- myPuasa/Home/HomeView.swift
+++ myPuasa/Home/HomeView.swift
@@ -10,9 +10,108 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         VStack {
-            Text("Home View")
+            HStack{
+                Text("Welcome back Harith")
+                    .bold()
+                    .padding(.top)
+                    .frame(maxWidth: .infinity, alignment: .center)
+                Spacer()
+            }
+            .padding(.bottom)
+            
+            
+            
+            
+            VStack{
+                HStack{
+                    HStack {
+                        ZStack{
+                            
+                            Circle()
+                                .foregroundStyle(Color.black)
+                                .frame(width: 70, height: 70)
+                            Circle()
+                                .foregroundStyle(Color.white)
+                                .frame(width: 60, height: 70)
+                            Text("7/14")
+                        }
+                        
+                        
+                        .font(.subheadline)
+                        Text("Keep it up, don't give up!")
+                            .padding(.leading)
+                            .font(.subheadline)
+                        Spacer()
+                        
+                    }
+                    .padding()
+                    .background(.pink, in:RoundedRectangle(cornerRadius: 15))
+                    .frame(maxWidth: .infinity, alignment: .center)
+                    
+                }
+                .font(.title)
+                
+                VStack {
+                    HStack{
+                        ZStack{
+                            Circle()
+                                .foregroundStyle(Color.black)
+                                .frame(width: 350)
+                            Circle()
+                                .foregroundStyle(Color.white)
+                                .frame(width: 260)
+                            Text("Cycle Day")
+                                .foregroundStyle(Color.black)
+                                .bold()
+                            
+                        }
+                        
+                    }
+                    .foregroundStyle(.blue)
+                    
+                    VStack{
+                        
+                        Text("Lorem")
+                            .padding()
+                        HStack{
+                            Rectangle()
+                                .frame(width: 30, height: 30)
+                            Rectangle()
+                                .frame(width: 30, height: 30)
+                            Rectangle()
+                                .frame(width: 30, height: 30)
+                            Rectangle()
+                                .frame(width: 30, height: 30)
+                        }
+                    }
+                    
+                }
+                .padding()
+                .background(.red)
+                
+            }
+            
+            
+            
+            HStack{
+                VStack{
+                    HStack{
+                        Text("Fidyah Guideline")
+                            .font(.title2)
+                            .padding()
+                        
+                    }
+                    
+                    
+                }
+                .font(.title3)
+            }
+            
+            Spacer()
         }
-        Rectangle()
+        
+        
+        
     }
 }
 
